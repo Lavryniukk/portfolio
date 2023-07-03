713f182b92c1793b0ae48d8b84166483e1c3c661
Lavryniuk Lev <leoyt109@gmail.com>
2023-07-03 15:59:44 +0200

    Added a load anim for Selfie and Introduction

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 window.addEventListener("load", () => {
   let links = document.querySelectorAll(".nav__link");
+  let selfPic = document.querySelector(".self-pic");
+  let bio = document.querySelector(".intro");
+  /*---Navigation animation---*/
   let links_arr = [...links];
-  let range = 0; // Set the desired value for the range
   links_arr.forEach((link, index) => {
     setTimeout(() => {
-      link.style.top = `${range}%`;
-    }, 400 * index);
+      link.style.top = `0%`;
+    }, 300 * index);
   });
+  /*---Selfie animation---*/
+  selfPic.style.left = "0%"
+  selfPic.style.position = "relative"
+  bio.style.right = "0%"
+  bio.style.position = "relative"
+  setTimeout(()=>{bio.style.boxShadow = "10px 10px 0px 5px var(--white-color)"},800)
 });
+
