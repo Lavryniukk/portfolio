836a539ff287a697ee0c4cbece76d951305fea24
Lavryniuk Lev <leoyt109@gmail.com>
2023-07-03 16:43:58 +0200

    Added header hide animation

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", (event) => {
   let links = document.querySelectorAll(".nav__link");
   let selfPic = document.querySelector(".self-pic");
   let bio = document.querySelector(".intro");
+
   /*---Navigation animation---*/
   let links_arr = [...links];
   links_arr.forEach((link, index) => {
@@ -9,11 +10,25 @@ window.addEventListener("load", () => {
       link.style.top = `0%`;
     }, 300 * index);
   });
-  /*---Selfie animation---*/
+  event.pageYOffset = 0
+  /*---Selfie and Bio animation ---*/
   selfPic.style.left = "0%"
   selfPic.style.position = "relative"
   bio.style.right = "0%"
   bio.style.position = "relative"
   setTimeout(()=>{bio.style.boxShadow = "10px 10px 0px 5px var(--white-color)"},800)
+});
+
+
+window.addEventListener("scroll", () => {
+	let pixelCounter = document.querySelector("p.pxc");
+	let header = document.querySelector("#header");
+	let scrolledPixels = window.pageYOffset || document.documentElement.scrollTop;
+	pixelCounter.innerText = `Scrolled: ${scrolledPixels}`;
+	if(scrolledPixels>685){
+		header.style.transform = "translateY(-100%)"
+	}else{
+		header.style.transform = 'translateY(0%)'
+	}
 });
 
