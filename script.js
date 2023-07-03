cb7ef51d271f3773730fde39d10b807baca5dce1
Lavryniuk Lev <leoyt109@gmail.com>
2023-07-03 14:51:07 +0200

    Added a load anim for navigation

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let links = document.querySelectorAll(".nav__link");
-let links_arr = [...links]
+window.addEventListener("load", () => {
+  let links = document.querySelectorAll(".nav__link");
+  let links_arr = [...links];
+  let range = 0; // Set the desired value for the range
+  links_arr.forEach((link, index) => {
+    setTimeout(() => {
+      link.style.top = `${range}%`;
+    }, 400 * index);
+  });
+});
